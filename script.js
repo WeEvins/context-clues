1abfc6b8a1122c75d813a1580b5026a9a68c67f6
Warryn Evins <W.Hatmaker@yahoo.com>
2018-06-07 14:11:39 -0500

    still tweaking the code

--- script.js
+++ script.js
@@ -7,15 +7,24 @@ document.addEventListener('DOMContentLoaded', function() {
     var weaponNames = ['bag of endless cats','candlejacks candlejack','15 toothpicks and a small amount of lint','a gun that shoots knives that explodes into smaller guns','just a lot of body odor. like, so much.','POCKET SAND','a really cool switchblade comb','the power of friendship','a really big sword. like jrpg big.','gordon freemans crowbar','an overly long anecdote','a bunch of dynamite and an acme plunger','a ton of tiny plastic horses','questionable gas station sushi','weapon','weapon2','weapon3','weapon4','weapon5'];
 
 
-    //I know i need to do a for loop that iterates to 100, and creates the h3s, appends them to the page, and 
+    //I know i need to do a for loop that iterates to 100, and creates the h3s, appends them to the page, 
     //then set an event listener on those individual h3 to give it a random accusation
-    var i = 0;
+    
 
+for (var i = 0; i < 101; i++) {
     var accusation = document.createElement('h3')
-    i = i + 1;
-    h3.innertext = `Accusation ${i}`;
-    i++;
-    document.appendChild(accusation);
+    f = i + 1;
+    accusation.createTextNode = `Accusation ${f}`; 
+   document.body.appendChild(accusation); //something isnt appending correctly to show on the page :/
 
+   //Okay, so i need to make a math for randomizing my friend, location, and weapon, then feed that
+   //to the double click func below vvvv 
+
+    
+
+   // accusation.addEventListener('dblclick' function(e){
+   //     var targetitem = e.target
+   //     targetitem.alert(`I accuse ${friend}, with the ${weapon} in the {$location}!!`)
+    }
 
 });    
