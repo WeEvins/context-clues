886094fa704193fec2c6212c198f9de121fe045d
Warryn Evins <W.Hatmaker@yahoo.com>
2018-06-07 13:44:17 -0500

    adding in the h3 creation

--- script.js
+++ script.js
@@ -7,9 +7,15 @@ document.addEventListener('DOMContentLoaded', function() {
     var weaponNames = ['bag of endless cats','candlejacks candlejack','15 toothpicks and a small amount of lint','a gun that shoots knives that explodes into smaller guns','just a lot of body odor. like, so much.','POCKET SAND','a really cool switchblade comb','the power of friendship','a really big sword. like jrpg big.','gordon freemans crowbar','an overly long anecdote','a bunch of dynamite and an acme plunger','a ton of tiny plastic horses','questionable gas station sushi','weapon','weapon2','weapon3','weapon4','weapon5'];
 
 
+    //I know i need to do a for loop that iterates to 100, and creates the h3s, appends them to the page, and 
+    //then set an event listener on those individual h3 to give it a random accusation
     var i = 0;
-//I know i need to do a for loop that iterates to 100, and creates the h3s, appends them to the page, and 
-//then set an event listener on those individual h3 to give it a random accusation
+
+    var accusation = document.createElement('h3')
+    i = i + 1;
+    h3.innertext = `Accusation ${i}`;
+    i++;
+    document.appendChild(accusation);
 
 
 });    
