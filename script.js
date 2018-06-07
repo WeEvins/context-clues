503c1ae082af86f7bee3fc3d0f0978ef24a373a2
Warryn Evins <W.Hatmaker@yahoo.com>
2018-06-07 13:14:05 -0500

    added dom event listener to page
    added some notes for myself

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-var friendNames = ['Klutch','Calvin Jitter','Riska','Frills','Kali'];
-var locationNames = ['ballpit','dining room','room with the moose','upstairs bathroom, you know the one for when you have too much taco bell','library','kitchen','guest bedroom','fancy parlor','normal parlor','pool in the back'];
-var weaponNames = ['bag of endless cats','candlejacks candlejack','15 toothpicks and a small amount of lint','a gun that shoots knives that explodes into smaller guns','just a lot of body odor. like, so much.','POCKET SAND','a really cool switchblade comb','the power of friendship','a really big sword. like jrpg big.','gordon freemans crowbar','an overly long anecdote','a bunch of dynamite and an acme plunger','a ton of tiny plastic horses','questionable gas station sushi','weapon','weapon2','weapon3','weapon4','weapon5'];
+
+document.addEventListener('DOMContentLoaded', function() {
+    
+    //defining my variables in one place for my convenience
+    var friendNames = ['Klutch','Calvin Jitter','Riska','Frills','Kali'];
+    var locationNames = ['ballpit','dining room','room with the moose','upstairs bathroom, you know the one for when you have too much taco bell','library','kitchen','guest bedroom','fancy parlor','normal parlor','pool in the back'];
+    var weaponNames = ['bag of endless cats','candlejacks candlejack','15 toothpicks and a small amount of lint','a gun that shoots knives that explodes into smaller guns','just a lot of body odor. like, so much.','POCKET SAND','a really cool switchblade comb','the power of friendship','a really big sword. like jrpg big.','gordon freemans crowbar','an overly long anecdote','a bunch of dynamite and an acme plunger','a ton of tiny plastic horses','questionable gas station sushi','weapon','weapon2','weapon3','weapon4','weapon5'];
+
+
+    var i = 0;
+//I know i need to do a for loop that iterates to 100, and creates the h3s, appends them to the page, and 
+//then set an event listener on those individual h3 to give it a random accusation
+
+
+});    
